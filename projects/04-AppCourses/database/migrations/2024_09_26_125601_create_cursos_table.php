022151b68241c924680f2236c74f91450d78882d
Lautaro Gallo <lautarovg147@gmail.com>
2024-09-27 10:05:09 -0300

    feature: Adding news migrations

--- projects/04-AppCourses/database/migrations/2024_09_26_125601_create_cursos_table.php
+++ projects/04-AppCourses/database/migrations/2024_09_26_125601_create_cursos_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();// created_at and updated_at
+            $table->text('description');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cursos');
     }
 };
